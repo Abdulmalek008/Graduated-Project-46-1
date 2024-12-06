ccbf4b698530b2c93d2ff0e264151d8c7c39241b
Abdulmalek008 <442811086@kku.edu.sa>
2024-12-07 00:16:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,22 +95,22 @@ st.dataframe(df_prediction_proba,
                  'A',
                  format='%f',
                  width='medium',
-                 min_value=80,
-                 max_value=100
+                 min_value=0,
+                 max_value=1
                ),
                'B':st.column_config.ProgressColumn(
                  'B',
                  format='%f',
                  width='medium',
-                 min_value=70,
-                 max_value=80
+                 min_value=0,
+                 max_value=1
                ),
                'C':st.column_config.ProgressColumn(
                  'C',
                  format='%f',
                  width='medium',
                  min_value=0,
-                 max_value=60
+                 max_value=1
                ),
              }, hide_index=True)
 
