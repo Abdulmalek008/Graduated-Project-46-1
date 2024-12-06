567da0bb54b59cb158877942785cb866ff420a9c
Abdulmalek008 <442811086@kku.edu.sa>
2024-12-06 19:33:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-with st.expander('البيانات'):
-  st.write('**بيانات الطلاب**')
+with st.expander('Data'):
+  st.write('**Raw Data*')
   df = pd.read_csv('https://raw.githubusercontent.com/Abdulmalek008/Graduated-Project-46-1/refs/heads/master/Student_Info%202.csv')
   df
 
@@ -19,3 +19,6 @@ X
 st.write('**y**')
 y = df.Level
 y
+
+with st.expander('Data visualization"):
+ st.scatter_chart(data=df , x="Total", y='Final_Score', color="Level') 
