5a7bf362838701b7767ec3ce20eb5f41d036cde1
Abdulmalek008 <442811086@kku.edu.sa>
2024-12-06 19:17:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-with st.expander('Data'):
-  st.write('**Raw data**')
+with st.expander('البيانات'):
+  st.write('**بيانات الطلاب**')
   df = pd.read_csv('https://raw.githubusercontent.com/Abdulmalek008/Graduated-Project-46-1/refs/heads/master/Student_Info%202.csv')
   df
 
